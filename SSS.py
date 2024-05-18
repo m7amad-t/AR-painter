19409f7880485a6f78ff6d8139051cd657e94736
Mohammad <mhamadtahir2003@gmail.com>
2024-05-19 02:18:12 +0300

    init phase

--- SSS.py
+++ SSS.py
@@ -1,13 +1,11 @@
-import cv2  # Import OpenCV
-import mediapipe as mp  # Import MediaPipe for hand tracking
-import numpy as np  # Import NumPy
-import screeninfo
+import cv2
+import mediapipe as mp
+import numpy as np
 
 
-# Get the full screen resolution
-screen = screeninfo.get_monitors()[0]  # Get the primary screen
-screen_width = 640
-screen_height = 480
+
+width = 640
+height = 480
 
 
 
@@ -16,8 +14,10 @@ screen_height = 480
 hands = mp.solutions.hands.Hands(
     min_detection_confidence=0.6,
     min_tracking_confidence=0.6,
-    max_num_hands=1  # Only track one hand
+    max_num_hands=1
 )
+
+
 drawing_utils = mp.solutions.drawing_utils  # For drawing landmarks
 
 # Constants for drawing
@@ -36,20 +36,16 @@ current_color = color_options['r']
 
 # Create a mask to draw on
 # Create a canvas for drawing with full-screen size
-mask = np.ones((screen_height, screen_width, 3), dtype=np.uint8) * 0  # White canvas
+mask = np.ones((height, width, 3), dtype=np.uint8) * 0
 
 previous_x, previous_y = None, None  # Previous drawing position
 
 # Webcam setup
 cap = cv2.VideoCapture(0)
 
+cap.set(3 , width)
+cap.set(4 , height)
 
-# Function to check if the index finger is raised
-def is_index_raised(hand_landmarks):
-    # Get the y-coordinates of the index and middle fingers
-    index_y = hand_landmarks.landmark[8].y * 480
-    middle_y = hand_landmarks.landmark[12].y * 480
-    return (middle_y - index_y) > 40  # If middle_y is higher, the index is raised
 
 def count_raised_fingers(landmarks):
     # Indices for fingertips and corresponding knuckles
@@ -93,6 +89,8 @@ while True:
     if not ret:
         break
 
+
+
     frame = cv2.flip(frame, 1)  # Flip the frame for a mirror effect
 
     # Convert the frame to RGB for MediaPipe processing
@@ -101,20 +99,23 @@ while True:
     # Process the frame to detect hand landmarks
     results = hands.process(rgb_frame)
 
+
+
     if results.multi_hand_landmarks:
+
+
         # If there are hand landmarks detected
         for hand_landmarks in results.multi_hand_landmarks:
+
+
             # Draw landmarks for visualization
             drawing_utils.draw_landmarks(frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
             # Get the index fingertip position
             index_x = int(hand_landmarks.landmark[8].x * 640)
             index_y = int(hand_landmarks.landmark[8].y * 480)
 
-
-
             # Count the number of raised fingers
             num_fingers = count_raised_fingers(hand_landmarks)
-            print(num_fingers)
             if num_fingers == 2:
                 # means the index finger is raised so draw
                 if previous_x is not None and previous_y is not None:
@@ -125,7 +126,7 @@ while True:
                 previous_x, previous_y = index_x, index_y
             elif num_fingers > 3 :
                 # Erase by drawing white circles on the canvas
-                cv2.circle(mask, (index_x, index_y), 30, (0, 0, 0), -1)  # Erasing with white
+                cv2.circle(mask, (index_x, index_y), 30, (0, 0, 0), -1)  # Erasing on canvas white
                 cv2.circle(frame, (index_x, index_y), 30, (255, 255, 255), -1)  # Erasing with white
                 previous_x, previous_y = None, None  # Reset previous point
             elif num_fingers == 3 :
@@ -136,29 +137,8 @@ while True:
             else :
                 previous_x, previous_y = None , None
 
-            # if is_index_raised(hand_landmarks):  # If the index finger is raised
-            #     if previous_x is not None and previous_y is not None:
-            #         # Draw a line on the mask with the current color
-            #         cv2.line(mask, (previous_x, previous_y), (index_x, index_y), current_color, DRAW_THICKNESS)
-            #
-            #     # Update previous coordinates
-            #     previous_x, previous_y = index_x, index_y
-            # else:
-            #     # Reset previous coordinates when index finger is not raised
-            #     previous_x, previous_y = None, None
-
-    # Apply the mask to the original frame
-
-
-    # op = cv2.bitwise_and(frame, frame, mask=mask)  # Apply the mask
-
-    # overlayed_frame = cv2.addWeighted(frame, 1, mask, 0.3, 0)
-    # frame[:, :, 1] = op[:, :, 1]
-    # frame[:, :, 2] = op[:, :, 2]
 
     merged_frame = cv2.add(frame , mask)
-    # Display the final frame with augmented reality drawing
-    cv2.imshow("Drawing App", merged_frame)
 
     # Display instructions for changing colors
     cv2.putText(merged_frame, "Current Color", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
